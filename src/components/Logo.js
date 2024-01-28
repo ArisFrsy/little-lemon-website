0139c2461df291355ed143a2a0e54d54cf770802
Firman <aris.it.dev@gmail.com>
2024-01-28 14:24:08 +0700

    add : little lemon website

---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Logo() {
+  return (
+    <img
+      className="logo"
+      src="https://png.pngtree.com/png-clipart/20200727/original/pngtree-lemon-logo-vector-design-icon-png-image_5212486.jpg"
+      alt="Lemon Logo"
+    />
+  );
+}
